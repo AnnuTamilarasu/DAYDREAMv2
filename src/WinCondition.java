86860980f5025044100f18c4586954124e581f29
AnnuTamilarasu <ananya.tamilarasu@gmail.com>
2025-09-28 13:45:13 -0400

    initial commit

--- src/WinCondition.java
+++ src/WinCondition.java
@@ -1,10 +1,6 @@
 import javax.swing.*;
 
 public class WinCondition extends JFrame{
-    public static void main(String[] args) {
-        new WinCondition();
-    }
-
     public WinCondition() {
         JOptionPane.showMessageDialog(null, "You won! Sacrifices must be made", "Win",
                 JOptionPane.INFORMATION_MESSAGE);
